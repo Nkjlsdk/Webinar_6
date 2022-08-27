11b404cb939851780e894e70889ac858fc0ab2b2
Your Name <e@w.com>
2022-08-27 04:25:43 +0300

    Ввод пяти чисел и вывод числа положительных

---
+++ Task_41/Program.cs
@@ -0,0 +1,13 @@
+﻿// Ввод с клавиатуры М чисел и вывысти сколько чисел больше 0 ввёл пользователь.
+
+int [] arrayM = new int [5];
+int count = 0;
+Console.WriteLine("Введите 5 положельных или отрицательных чисел");
+
+for (int i = 0; i < arrayM.Length; i++)
+{
+        arrayM[i]=Convert.ToInt32(Console.ReadLine());
+        if (arrayM[i] > 0) count++;
+    
+}
+Console.WriteLine($"количество положительных чисел: {count}");
