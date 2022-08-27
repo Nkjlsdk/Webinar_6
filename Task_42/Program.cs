b5a79b44595c5764ac65ab59cb0ec93752d139e9
Your Name <e@w.com>
2022-08-27 04:28:44 +0300

    Преобразование 10го числа в двоичное

---
+++ Task_42/Program.cs
@@ -0,0 +1,19 @@
+﻿// Ввод числа и преобразование десятичного числа в двоичное.
+//45 -> 101101
+//3 -> 11
+//2 -> 10
+
+ 
+            Console.Write("Введите число: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            string result = string.Empty;
+            while (number > 0)
+            {
+                int remain = number % 2;
+                number = number/2;
+                result = remain.ToString() + result;
+            }
+            Console.WriteLine($"Двоичное число: {result}");
+               
+    
